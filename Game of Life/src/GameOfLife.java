eb87157d6df80c2af46e875f19a644a116825cdf
Luis Aguirre <luis.alejandro.aguirre@outlook.com>
2020-02-13 23:58:14 -0700

    Initial commit. GameOfLife.java and TorusGameOfLife.java completed.

--- Game of Life/src/GameOfLife.java
+++ Game of Life/src/GameOfLife.java
@@ -13,8 +13,13 @@ public class GameOfLife {
     }
 
     GameOfLife(int[][] board){
-        this.board = board;
-        previous = board;
+        this.board = new int[board.length][board[0].length];
+        previous = new int[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                previous[i][j] = board[i][j];
+            }
+        }
     }
 
     public int[][] getBoard() {
@@ -22,16 +27,58 @@ public class GameOfLife {
     }
 
     public void oneStep(){
-        //TODO: Finish method
+        for(int i = 0; i < previous.length; i++){
+            for(int j = 0; j < previous[i].length; j++){
+                int liveNeighbors = neighbors(i,j);
+                if(previous[i][j] == 1 && liveNeighbors < 2){
+                    board[i][j] = 0;
+                } else if(previous[i][j] == 1 && liveNeighbors > 3){
+                    board[i][j] = 0;
+                } else if(previous[i][j] == 0 && liveNeighbors == 3){
+                    board[i][j] = 1;
+                } else{
+                    board[i][j] = previous[i][j];
+                }
+            }
+        }
+
+        for(int i = 0; i < previous.length; i++){
+            for(int j = 0; j < previous[i].length; j++){
+                previous[i][j] = board[i][j];
+            }
+        }
+        System.out.println();
+
+        printBoard();
     }
 
     public int neighbors(int row, int column){
-        //TODO: Finish method
+        int neighbors = 0;
+        for(int i = row - 1; i <= row + 1; i++){
+            for(int j = column - 1; j <= column + 1; j++){
+                if((i < 0) || (i > previous.length - 1) || (j < 0) || (j > previous[i].length - 1)){
+                    continue;
+                }
+                neighbors += previous[i][j];
+            }
+        }
+        neighbors -= previous[row][column];
+        return neighbors;
     }
 
     public void evolution(int n){
-        //TODO: Finish method
+        while(n > 0){
+            oneStep();
+            --n;
+        }
     }
 
-
+    public void printBoard(){
+        for(int i = 0; i < previous.length; i++){
+            for(int j = 0; j < previous[i].length; j++){
+                System.out.print(board[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
 }
--- Game of Life/src/TorusGameOfLife.java
+++ Game of Life/src/TorusGameOfLife.java
@@ -4,22 +4,29 @@ public class TorusGameOfLife extends GameOfLife{
     }
 
     TorusGameOfLife(int s){
-        size = s;
+        board = new int[size][size];
+        previous = new int[size][size];
     }
 
     TorusGameOfLife(int[][] board){
-        this.board = board;
+        this.board = new int[board.length][board[0].length];
+        previous = new int[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                previous[i][j] = board[i][j];
+            }
+        }
     }
 
-    public void oneStep(){
-        // TODO finish method
-    }
-
+    @Override
     public int neighbors(int row, int column){
-        // TODO Finish method
-    }
-
-    public int evolution(int n){
-        // TODO Finish method
+        int neighbors = 0;
+        for(int i = row - 1; i <= row + 1; i++){
+            for(int j = column - 1; j <= column + 1; j++){
+                neighbors += previous[(i % board.length + board.length) % board.length][(j % board[0].length + board[0].length) % board[0].length];
+            }
+        }
+        neighbors -= previous[row][column];
+        return neighbors;
     }
 }
